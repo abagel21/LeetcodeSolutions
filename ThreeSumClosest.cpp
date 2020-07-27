4569bd3edf19817351db2565aee57bef4d74a42a
Alex Nagel <slipshot21@gmail.com>
2020-07-26 23:18:12 -0500

    added threesumclosest

---
+++ ThreeSumClosest.cpp
@@ -0,0 +1,24 @@
+#include <vector>
+#include <algorithm>
+using namespace std;
+class Solution {
+public:
+    int threeSumClosest(vector<int>& nums, int target) {
+        int closesum = INT_MAX - abs(target) - 1;
+        sort(nums.begin(), nums.end());
+        for (int i = 0; i < nums.size(); i++) {
+            int j = i + 1, k = nums.size() - 1, sum;
+            while (j < k) {
+                sum = nums[i] + nums[j] + nums[k];
+                if (abs(target - sum) < abs(target - closesum)) closesum = sum;
+                if (sum < target) j++;
+                else if (sum > target) k--;
+                else {
+                    return sum;
+                }
+            }
+        }
+        return closesum;
+    }
+};
+
